af2560bbc990dec6c31269b72d03f7f5e643405a
nadayassin19 <yassinada19@gmail.com>
2023-01-15 18:07:45 +0200

    addedAPI

--- testCode/src/app/gym.service.ts
+++ testCode/src/app/gym.service.ts
@@ -12,10 +12,10 @@ export class GymService {
 
   { }
   getGym(id: string) {
-    return this.http.get<gym>(id);
+    return this.http.get<gym>('https://schema.postman.com/json/collection/v2.0.0/collection.json'+id);
   }
 
   getGyms(){
-    return this.http.get<gym[]>();
+    return this.http.get<gym[]>('https://schema.postman.com/json/collection/v2.0.0/collection.json');
   }
 }
